9d26ef2910aad36f3146d59513c34e3e99bdd932
Ayush Dama <ayushdama07@gmail.com>
2025-01-22 15:12:26 +0530

    modifying budget, members

--- yatrazen/src/components/ui/custom/Header.jsx
+++ yatrazen/src/components/ui/custom/Header.jsx
@@ -4,9 +4,10 @@ import { Button } from "../ui/button";
 function Header() {
   return (
     <div className="w-screen p-1 shadow-sm flex justify-between items-center px-5 bg-white">
-      <img src="/yatrazen.svg" alt="YatraZen Logo" className="h-12 sm:h-16 md:h-20 rounded-xl" />
+      <h1 className="h-8 sm:h-15 md:h-16 rounded-xl text-[#FF3B30] font-bold">yatrazen!!</h1>
+      {/* <img src="/yatrazen.svg" alt="YatraZen Logo" className="h-12 sm:h-16 md:h-20 rounded-xl" /> */}
       <div>
-        <Button>Sign In</Button>
+        <Button className='mr-2'>Sign In</Button>
       </div>
     </div>
   );
--- yatrazen/src/create-trip/index.jsx
+++ yatrazen/src/create-trip/index.jsx
@@ -1,29 +1,82 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { SelectBudgetOptions, SelectTravelsList } from '../constants/options';
 
 const apiKey = import.meta.env.VITE_GOOGLE_PLACE_API_KEY; // Fixed API key import
 
 function CreateTrip() {
   const [place, setPlace] = useState();
+
+  const [formData, setFormData] = useState([]);
+
+  const handleInputChange = (name,value)=>{
+    setFormData({
+      ...formData,
+      [name] : value
+    })
+  }
+
+  useEffect(()=>{
+    console.log(formData);
+  }, [formData])
+
   return (
-    <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
-      <h2 className='font-bold text-3xl'>Tell us your travel preferences</h2>
+    <div className='sm:px-10 md:px-32 lg:px-56 xl:px-72 px-5 mt-10 items-center w-screen'>
+      <h2 className='font-bold text-3xl'>Tell us your travel preferences 🚙🌴</h2>
       <p className='mt-3 text-gray-500 text-xl'>Just provide some basic information, and our trip planner will generate a customized itinerary based on your preferences</p>
 
-      <div className='mt-20'>
+      <div className='mt-20 flex flex-col gap-10'>
         <div>
           <h2 className='text-xl my-3 font-medium'>What is your destination of choice?</h2>
           <GooglePlacesAutocomplete 
             apiKey={apiKey} 
             selectProps = {{  
               place,  // stores the selected location
-              onChange:(v)=>{setPlace(v);console.log(v)}
+              onChange:(v)=>{
+                setPlace(v);
+                handleInputChange('location',v);
+              }
             }}
           /> 
         </div>
+
+        <div>
+          <h2 className='text-xl my-3 font-medium'>How many days are you planning your trip</h2>
+          <Input placeholder={'Ex.3'} type="number" />
+        </div>
+            
+        <div>
+          <h2 className='text-xl my-3 font-medium'>What is Your Budget?</h2>
+          <div className='grid grid-cols-3 gap-5 mt-5'>
+            {SelectBudgetOptions.map((item,index)=>(
+              <div key={index} className='p-4 border rounded-lg hover:shadow-lg'>
+                <h2 className='text-4xl'>{item.icon}</h2>
+                <h2 className='font-bold text-lg'>{item.title}</h2>
+                <h2 className='text-sm text-gray-500'>{item.desc}</h2>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div>
+          <h2 className='text-xl my-3 font-medium'>Who do you plan on travelling with on your next adventure?</h2>
+          <div className='grid grid-cols-3 gap-5 mt-5'>
+            {SelectTravelsList.map((item,index)=>(
+              <div key={index} className='p-4 border rounded-lg hover:shadow-lg'>
+                <h2 className='text-4xl'>{item.icon}</h2>
+                <h2 className='font-bold text-lg'>{item.title}</h2>
+                <h2 className='text-sm text-gray-500'>{item.desc}</h2>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
 
-      
+      <div className='my-10 justify-end flex'>
+        <Button>Generate Trip</Button>
+      </div>
     </div>
   );
 }
