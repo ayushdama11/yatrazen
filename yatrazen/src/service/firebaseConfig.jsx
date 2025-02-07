038b8d04625940eed0a4ae07ac0edbf4a42a4dc5
Ayush Dama <ayushdama07@gmail.com>
2025-02-07 21:50:20 +0530

    configs

--- yatrazen/src/service/firebaseConfig.jsx
+++ yatrazen/src/service/firebaseConfig.jsx
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCwE9LoB83vlq61yzfD_F9mRaZpMWoWDB8",
-  authDomain: "yatrazen-f1c6b.firebaseapp.com",
-  projectId: "yatrazen-f1c6b",
-  storageBucket: "yatrazen-f1c6b.appspot.com",
-  messagingSenderId: "783368020271",
-  appId: "1:783368020271:web:4ca5d197db15f52ffc41da",
-  measurementId: "G-NN1YMDEG45"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
